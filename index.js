712dfd5e5c33b164441e7069d7aa77daab5f0896
vishal s <vishals@gmai.com>
2023-01-02 17:42:34 +0530

    index.js added arrow functions

---
+++ index.js
@@ -0,0 +1,15 @@
+const add = (a,b) => {
+    return a + b;
+}
+
+const div = (a,b) => {
+    return a / b;
+}
+
+const substract = (a,b) => {
+    return a-b;
+}
+
+let str = `  Lorem ipsum, dolor sit amet consectetur adipisicing elit Sapiente neque alias,
+ commodi quis delectus odit ab provident 
+ facilis minima asperiores ea in sit dicta earum iste quo magnam accusantium autem.` ;
